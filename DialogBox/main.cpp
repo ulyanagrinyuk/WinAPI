af11c7088681c2b54901f597f20a2f4c9b6b509c
Uliana.29 <Uliana.29@DESKTOP-6DD5K9D>
2023-10-23 15:37:19 +0300

    Encoding

--- DialogBox/main.cpp
+++ DialogBox/main.cpp
@@ -1,6 +1,8 @@
 #include<Windows.h>
 #include"resource.h"
 
+CONST CHAR g_sz_LOGIN_INVITATION[] = "¬ведите им€ пользовател€";
+
 BOOL CALLBACK DlgProc(HWND hwnd, UINT msg, WPARAM wPram, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hprevInst, LPSTR lpCmdLine, INT nCmdShow)
@@ -13,16 +15,32 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wPram, LPARAM lParam)
 {
 	switch (uMsg)
 	{
+	case IDC_EDIT_LOGIN:
+	{
+		CONST INT SIZE = 256;
+		CHAR sz_buffer[SIZE]{};
+		HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+		SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+		if (HIWORD(wParam) == EN_SETFOCUS && strcmp(sz_buffer, g_sz_LOGIN_INVITATION) == 0)
+			SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)"");
+		if (HIWORD(wParam) == EN_KILLFOCUS && strcmp(sz_buffer, "") == 0)
+			SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)g_sz_LOGIN_INVITATION);
+	}
+		break;
 	case WM_INITDIALOG:
 	{
 		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
 		SendMessage(hwnd, WM_SETICON, 0, (LRESULT)hIcon);
-		SetFocus(GetDlgItem(hwnd, IDC_EDIT_LOGIN));
+		//SetFocus(GetDlgItem(hwnd, IDC_EDIT_LOGIN));
+		HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+		SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)g_sz_LOGIN_INVITATION);
+
 	}
 	break;
 	case WM_COMMAND:
 		switch (LOWORD(wPram))
 		{
+			
 		case IDC_BUTTON_COPY:
 		{		
 			CONST INT SIZE = 256;
